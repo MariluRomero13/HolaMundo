722f6aaa7cc538d623da0d5de8272d773b9da25f
marilu <romeromarilu13@gmail.com>
2019-01-10 08:07:33 -0600

    Agregué un toast

--- app/src/main/java/com/example/maril/holamundo/MainActivity.java
+++ app/src/main/java/com/example/maril/holamundo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         f.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Toast.makeText(MainActivity.this, "Hola mundo", Toast.LENGTH_SHORT).show();
             }
         });
 
