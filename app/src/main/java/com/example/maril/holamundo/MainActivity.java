1fc573825199d0205b6e0c4cde0c16c93822a39b
marilu <romeromarilu13@gmail.com>
2019-01-10 08:04:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/maril/holamundo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.maril.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button f = findViewById(R.id.btnhola);
+
+        f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+    }
+}
